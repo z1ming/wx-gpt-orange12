3065be9df8df8a4eabffda87ec420a7123f7c554
mengziming021 <mengziming021@hellobike.com>
2023-05-26 11:32:53 +0800

    feat: update

--- handle.py
+++ handle.py
@@ -39,38 +39,22 @@ class Handle(object):
             return str(e)
 
     def POST(self):
-        # 主函数开始
-        def async_callback():
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(send_reply())
-
-        async def send_reply():
-            try:
-                webData = web.data()
-                print("Handle Post webdata is ", webData)
-                recMsg = receive_json.parse_json(webData)
-                if isinstance(recMsg, receive_json.Msg) and recMsg.MsgType == 'text':
-                    toUser = recMsg.FromUserName
-                    fromUser = recMsg.ToUserName
-                    content = recMsg.Content
-                    replyMsg = reply.TextMsg(toUser, fromUser, content)
-                    print('replyMsg: ', replyMsg.__dict__)
-                    print('异步调用前')
-
-                    await replyMsg.send()  # 异步调用
-
-                    print('异步调用后')
-            except Exception as e:
-                print(str(e))
-
-        # 先立即返回 "success"
-        response = "success"
-
-        # 在新线程中执行异步回调
-        threading.Thread(target=async_callback).start()
-
-        return response
+        try:
+            webData = web.data()
+            print("Handle Post webdata is ", webData)
+            recMsg = receive_json.parse_json(webData)
+            if isinstance(recMsg, receive_json.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = recMsg.Content
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                print('replyMsg: ', replyMsg.__dict__)
+                print('异步调用前')
+                threading.Thread(target=replyMsg.send()).start()
+                print('异步调用后')
+            return 'success'
+        except Exception as e:
+            print(str(e))
 
 
 
