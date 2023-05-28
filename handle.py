fbec52460baf391648f4eb4ea6c0e6453b532909
mengziming021 <mengziming021@hellobike.com>
2023-05-28 13:40:40 +0800

    feat: update

--- handle.py
+++ handle.py
@@ -2,15 +2,10 @@
 # filename: handle.py
 
 import hashlib
-import threading
-
 import web
 import reply
 import receive_json
 import asyncio
-import post
-import json
-
 
 class Handle(object):
     def GET(self):
@@ -38,7 +33,7 @@ class Handle(object):
         except Exception as e:
             return str(e)
 
-    def POST(self):
+    async def POST(self):
         try:
             webData = web.data()
             print("Handle Post webdata is ", webData)
@@ -62,3 +57,55 @@ async def send_reply(replyMsg):
     await replyMsg.send()
 
 
+
+
+# class Handle(object):
+#     def GET(self):
+#         try:
+#             data = web.input()
+#             if len(data) == 0:
+#                 return "hello, this is handle view"
+#             signature = data.signature
+#             timestamp = data.timestamp
+#             nonce = data.nonce
+#             echostr = data.echostr
+#             token = "mengziming021"  # 请按照公众平台官网\基本配置中信息填写
+#
+#             data_list = [token, timestamp, nonce]
+#             data_list.sort()
+#             sha1 = hashlib.sha1()
+#             for item in data_list:
+#                 sha1.update(item.encode('utf-8'))
+#             hashcode = sha1.hexdigest()
+#             print("handle/GET func: hashcode, signature: ", hashcode, signature)
+#             if hashcode == signature:
+#                 return echostr
+#             else:
+#                 return ""
+#         except Exception as e:
+#             return str(e)
+#
+#     def POST(self):
+#         try:
+#             webData = web.data()
+#             print("Handle Post webdata is ", webData)
+#             recMsg = receive_json.parse_json(webData)
+#             if isinstance(recMsg, receive_json.Msg) and recMsg.MsgType == 'text':
+#                 toUser = recMsg.FromUserName
+#                 fromUser = recMsg.ToUserName
+#                 content = recMsg.Content
+#                 replyMsg = reply.TextMsg(toUser, fromUser, content)
+#                 print('replyMsg: ', replyMsg.__dict__)
+#                 print('异步调用前')
+#                 asyncio.create_task(send_reply(replyMsg))  # 将协程添加到事件循环中，立即返回
+#                 print('异步调用后')
+#             return 'success'  # 立即返回'success'
+#         except Exception as e:
+#             print(str(e))
+#
+#
+# async def send_reply(replyMsg):
+#     # 在这里执行发送回复的逻辑
+#     await replyMsg.send()
+#
+#
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
 import asyncio
-
 import web
 from handle import Handle
-
-
-
 
 urls = (
     '/xcx', 'Handle',
@@ -14,7 +10,7 @@ urls = (
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
-    # 创建事件循环
     loop = asyncio.get_event_loop()
     asyncio.set_event_loop(loop)
     app.run()
+
