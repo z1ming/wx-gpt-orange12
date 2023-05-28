748280336c301e984df886bc794cdcdd57267c1c
mengziming021 <mengziming021@hellobike.com>
2023-05-28 14:06:08 +0800

    feat: update

--- handle.py
+++ handle.py
@@ -6,6 +6,7 @@ import web
 import reply
 import receive_json
 import asyncio
+from main import global_queue
 
 class Handle(object):
     def GET(self):
@@ -44,67 +45,8 @@ class Handle(object):
                 content = recMsg.Content
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 print('replyMsg: ', replyMsg.__dict__)
-                print('异步调用前')
-                # send_reply(replyMsg)
-                print('异步调用后')
+                # replyMsg.send()
+                global_queue.put(replyMsg)
             return 'success'  # 立即返回'success'
         except Exception as e:
             print(str(e))
-
-async def send_reply(replyMsg):
-    # 在这里执行发送回复的逻辑
-    await replyMsg.send()
-
-
-
-
-# class Handle(object):
-#     def GET(self):
-#         try:
-#             data = web.input()
-#             if len(data) == 0:
-#                 return "hello, this is handle view"
-#             signature = data.signature
-#             timestamp = data.timestamp
-#             nonce = data.nonce
-#             echostr = data.echostr
-#             token = "mengziming021"  # 请按照公众平台官网\基本配置中信息填写
-#
-#             data_list = [token, timestamp, nonce]
-#             data_list.sort()
-#             sha1 = hashlib.sha1()
-#             for item in data_list:
-#                 sha1.update(item.encode('utf-8'))
-#             hashcode = sha1.hexdigest()
-#             print("handle/GET func: hashcode, signature: ", hashcode, signature)
-#             if hashcode == signature:
-#                 return echostr
-#             else:
-#                 return ""
-#         except Exception as e:
-#             return str(e)
-#
-#     def POST(self):
-#         try:
-#             webData = web.data()
-#             print("Handle Post webdata is ", webData)
-#             recMsg = receive_json.parse_json(webData)
-#             if isinstance(recMsg, receive_json.Msg) and recMsg.MsgType == 'text':
-#                 toUser = recMsg.FromUserName
-#                 fromUser = recMsg.ToUserName
-#                 content = recMsg.Content
-#                 replyMsg = reply.TextMsg(toUser, fromUser, content)
-#                 print('replyMsg: ', replyMsg.__dict__)
-#                 print('异步调用前')
-#                 asyncio.create_task(send_reply(replyMsg))  # 将协程添加到事件循环中，立即返回
-#                 print('异步调用后')
-#             return 'success'  # 立即返回'success'
-#         except Exception as e:
-#             print(str(e))
-#
-#
-# async def send_reply(replyMsg):
-#     # 在这里执行发送回复的逻辑
-#     await replyMsg.send()
-#
-#
--- main.py
+++ main.py
@@ -3,10 +3,37 @@
 import web
 from handle import Handle
 
+import queue
+import threading
+
+# 创建全局队列
+global_queue = queue.Queue()
+
+# 消费者线程，从队列中获取元素进行消费
+def consumer():
+    while True:
+        if not global_queue.empty():
+            replyMsg = global_queue.get()
+            print("消费元素:", replyMsg)
+            replyMsg.send()
+
+        else:
+            # 队列为空，进行其他操作或休眠一段时间
+            print("队列为空，进行其他操作或休眠...")
+            # 在这里添加需要执行的操作或休眠的逻辑
+            # ...
+
 urls = (
     '/xcx', 'Handle',
 )
 
 if __name__ == '__main__':
+    # 创建并启动消费者线程
+    consumer_thread = threading.Thread(target=consumer)
+    # 启动消费者线程
+    consumer_thread.start()
+    # 启动生产者线程（假设您已经有了这个线程）
     app = web.application(urls, globals())
     app.run()
+    # 等待消费者线程结束
+    consumer_thread.join()
