366326d683898fa3bbbae79bf78eb9c7c61828f1
mengziming021 <mengziming021@hellobike.com>
2023-05-28 13:32:27 +0800

    feat: update

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
+import asyncio
+
 import web
 from handle import Handle
+
+# 创建事件循环
+loop = asyncio.get_event_loop()
+asyncio.set_event_loop(loop)
+
 
 urls = (
     '/xcx', 'Handle',
