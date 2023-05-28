4cf46d5937847f7a9cf579634844b62ef01b2c3e
mengziming021 <mengziming021@hellobike.com>
2023-05-28 13:36:12 +0800

    feat: update

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import asyncio
 import web
 from handle import Handle
 
-# 创建事件循环
-loop = asyncio.get_event_loop()
-asyncio.set_event_loop(loop)
+
 
 
 urls = (
@@ -16,4 +14,7 @@ urls = (
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
+    # 创建事件循环
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)
     app.run()
